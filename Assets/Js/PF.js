73c7fe2465f16da5181c9c1ff2fe12c87a4b0659
Clara Godoy <129601754+MaryGodoy@users.noreply.github.com>
2023-07-29 11:51:58 -0300

    27/07 e 29/07-> Projeto final
    Portfólio

--- Assets/Js/PF.js
+++ Assets/Js/PF.js
@@ -1,15 +1,14 @@
-const abrirJanela = document.querySelectorAll('.janela.abrir')
+const abrirJanela = document.querySelectorAll('.janela .abrir')
 
 abrirJanela.forEach((abrir) => {
-abrir.addEventListener('click', (e) => {
-    const janela = abrir.parentElement
-    const isOpen = janela.classList.contains('open')
+    abrir.addEventListener('click', (e) => {
+        const janela = abrir.parentElement
+        const isOpen = janela.classList.contains('open')
 
-    if (isOpen) {
-        janela.classList.remove('open')
-    }else{
-        janela.classList.add('open')
-    }
+        if (isOpen) {
+            janela.classList.remove('open')
+        } else {
+            janela.classList.add('open')
+        }
+    })
 })
-}
-)
